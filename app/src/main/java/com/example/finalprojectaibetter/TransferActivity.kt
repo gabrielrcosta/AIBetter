653000a83b23ed43557769d52482039f00353b16
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-01-29 16:22:23 +0000

    created functionality where on click the app closes to TransferActivity.kt and opens MainScreenActivity.

--- app/src/main/java/com/example/finalprojectaibetter/TransferActivity.kt
+++ app/src/main/java/com/example/finalprojectaibetter/TransferActivity.kt
@@ -1,11 +1,23 @@
 package com.example.finalprojectaibetter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class TransferActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
+
+        val closeButton: ImageView = findViewById(R.id.close_arrow)
+        closeButton.setOnClickListener {
+            openActivity()
+        }
+    }
+    private fun openActivity() {
+        val intent = Intent(this, MainScreenActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
