c13fe1d8dba052f922c604651d63c495f2880b54
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-01-31 15:16:21 +0000

    created new CardManagerActivity.kt and its .xml file, changes were made in login screen where the login text and password were not able to be seen

---
+++ app/src/main/java/com/example/finalprojectaibetter/CardManagerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojectaibetter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_manager)
+    }
+}
