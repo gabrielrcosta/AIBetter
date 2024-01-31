8eca78cf7830f691e5bab57e8613eb5c6e035083
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-01-31 16:31:10 +0000

    fake card image added and function to click on card icon to load cardactivity.

--- app/src/main/java/com/example/finalprojectaibetter/MainScreenActivity.kt
+++ app/src/main/java/com/example/finalprojectaibetter/MainScreenActivity.kt
@@ -10,11 +10,22 @@ class MainScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
 
-        val closeButton: ImageView = findViewById(R.id.tranfers_icon)
-        closeButton.setOnClickListener {
+        val transferButton: ImageView = findViewById(R.id.tranfers_icon)
+        transferButton.setOnClickListener {
             openActivity()
         }
+        val cardsButton: ImageView = findViewById(R.id.cards_icon)
+        cardsButton.setOnClickListener {
+            openCardActivity()
+        }
     }
+
+    private fun openCardActivity() {
+        val intent = Intent(this, CardManagerActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
     private fun openActivity() {
         val intent = Intent(this, TransferActivity::class.java)
         startActivity(intent)
