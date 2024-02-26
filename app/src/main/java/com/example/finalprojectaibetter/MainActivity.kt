ba85a3ead995356a991b9a04979f4e91b2439bd5
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-02-26 20:08:46 +0000

    created authentication to log into the app. added the binding feature.

--- app/src/main/java/com/example/finalprojectaibetter/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectaibetter/MainActivity.kt
@@ -3,23 +3,46 @@ package com.example.finalprojectaibetter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
+import com.example.finalprojectaibetter.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //initializing binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            openActivity()
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.button.setOnClickListener{
+            /*val regNumber = binding.regNumber.text.toString()
+            val password = binding.passwordEditable.text.toString()
+
+            if (regNumber.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(regNumber, password).addOnCompleteListener {
+                    if (it.isSuccessful) {*/
+                        val intent = Intent(this, MainScreenActivity::class.java)
+                        startActivity(intent)
+                    /*} else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }*/
         }
     }
-
-    private fun openActivity() {
-        val intent = Intent(this, MainScreenActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
+        /*override fun onStart() {
+            super.onStart()
+            //checking if user is logged in or not and if not, loading logIn page.
+            if (firebaseAuth.currentUser != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }*/
 }
