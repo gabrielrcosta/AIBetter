82df8c31d7962e23814ee376e5da9bf12829afde
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-03-21 11:49:50 +0000

    Created User class

---
+++ app/src/main/java/com/example/finalprojectaibetter/Model/User.kt
@@ -0,0 +1,7 @@
+package com.example.finalprojectaibetter.Model
+
+data class User(val userId: String,
+                val userFirstName: String,
+                val userLastName: String,
+                val email: String,
+                val accountId: String)
