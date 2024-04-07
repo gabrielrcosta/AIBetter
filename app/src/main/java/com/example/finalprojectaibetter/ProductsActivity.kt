a67958c9edbe5cae6f03822bf9cb68ab1b69a4b8
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-04-07 20:36:58 +0100

    Added functions of click navigation and created a class setupClickListener to easy access and clear the onCreate

--- app/src/main/java/com/example/finalprojectaibetter/ProductsActivity.kt
+++ app/src/main/java/com/example/finalprojectaibetter/ProductsActivity.kt
@@ -4,32 +4,37 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import com.example.finalprojectaibetter.databinding.ActivityProductsBinding
 
 class ProductsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_products)
-
-        val homeButton: ImageView = findViewById(R.id.home_balance_icon)
-        val transferButton: ImageView = findViewById(R.id.tranfers_icon)
-        val cardsButton: ImageView = findViewById(R.id.cards_icon)
-
-        homeButton.setOnClickListener {
+        binding = ActivityProductsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupClickListener()
+    }
+    private fun setupClickListener() {
+        //class created to clear the onCreate function and pass only one call.
+        binding.homeBalanceIcon.setOnClickListener {
             navigateTo(MainScreenActivity::class.java)
         }
-
-        transferButton.setOnClickListener {
+        binding.tranfersIcon.setOnClickListener {
             navigateTo(TransferActivity::class.java)
         }
-
-        cardsButton.setOnClickListener {
+        binding.cardsIcon.setOnClickListener {
             navigateTo(CardManagerActivity::class.java)
         }
+        binding.settingsIcon.setOnClickListener {
+            navigateTo(SettingsActivity::class.java)
+        }
+        binding.budgetButton.setOnClickListener {
+            navigateTo(BudgetForecastActivity::class.java)
+        }
     }
-}
 
     private fun Context.navigateTo(destination: Class<*>) {
         val intent = Intent(this, destination)
         startActivity(intent)
     }
+}
