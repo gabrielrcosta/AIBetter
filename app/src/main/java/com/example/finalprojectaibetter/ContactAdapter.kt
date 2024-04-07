4f3f054aa4d65017d39d298b90aa301658aac966
@gabrielrcosta <ribeirocosta.rj1@gmail.com>
2024-04-07 21:52:26 +0100

    created adapter to inflate the view

---
+++ app/src/main/java/com/example/finalprojectaibetter/ContactAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.finalprojectaibetter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalprojectaibetter.Model.User
+
+class ContactAdapter(private val contactsList: List<User>) : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+
+    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val firstName: TextView = itemView.findViewById(R.id.first_name)
+        val lastName: TextView = itemView.findViewById(R.id.last_name)
+        val roundImageView : RoundImageView = itemView.findViewById(R.id.round_image_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = contactsList[position]
+        holder.firstName.text = contact.userFirstName
+        holder.lastName.text = contact.userLastName
+        holder.roundImageView.loadImage(contact.profilePic)
+    }
+
+    override fun getItemCount(): Int = 3
+}
