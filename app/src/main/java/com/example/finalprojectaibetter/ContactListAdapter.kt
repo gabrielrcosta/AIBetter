249abf8c1ea9048c3ac7647abc15351970b3f337
gabrielrcosta <79279115+gabrielrcosta@users.noreply.github.com>
2024-08-05 01:18:39 +0100

    created interface receive the click data and operation

--- app/src/main/java/com/example/finalprojectaibetter/ContactListAdapter.kt
+++ app/src/main/java/com/example/finalprojectaibetter/ContactListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.finalprojectaibetter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,14 +8,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalprojectaibetter.Model.User
 
-class ContactListAdapter(private val contactList: MutableList<User>) :
-        RecyclerView.Adapter<ContactListAdapter.ContactListViewHolder>() {
-            class ContactListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-                val firstName: TextView = itemView.findViewById(R.id.first_name)
-                val lastName: TextView = itemView.findViewById(R.id.last_name)
-                val roundImageView : RoundImageView = itemView.findViewById(R.id.round_image_view)
-
+class ContactListAdapter(
+    private val contactList: MutableList<User>,
+    private val listener: OnContactItemClickListener
+) : RecyclerView.Adapter<ContactListAdapter.ContactListViewHolder>() {
+    class ContactListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val firstName: TextView = itemView.findViewById(R.id.first_name)
+        val roundImageView : RoundImageView = itemView.findViewById(R.id.round_image_view)
+        @SuppressLint("SetTextI18n")
+        fun bind(user: User, listener: OnContactItemClickListener) {
+            firstName.text = "${user.userFirstName} ${user.userLastName}"
+            itemView.setOnClickListener {
+                listener.onContactItemClick(user)
             }
+        }
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactListViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
         return ContactListViewHolder(view)
@@ -23,9 +31,13 @@ class ContactListAdapter(private val contactList: MutableList<User>) :
     override fun onBindViewHolder(holder: ContactListViewHolder, position: Int) {
         val contact = contactList[position]
         holder.firstName.text = contact.userFirstName
-        holder.lastName.text = contact.userLastName
         holder.roundImageView.loadImage(contact.profilePic)
+        holder.bind(contact, listener)
     }
 
     override fun getItemCount(): Int = contactList.size
+
+    interface OnContactItemClickListener {
+        fun onContactItemClick(contact: User)
+    }
 }
