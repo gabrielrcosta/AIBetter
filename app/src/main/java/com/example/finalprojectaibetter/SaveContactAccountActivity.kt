d061d6a439f1c6ab2bc68ae7fe1dd76f86b8f55e
gabrielrcosta <79279115+gabrielrcosta@users.noreply.github.com>
2024-08-05 01:02:47 +0100

    added functions to check contact list and treated the errors.

--- app/src/main/java/com/example/finalprojectaibetter/SaveContactAccountActivity.kt
+++ app/src/main/java/com/example/finalprojectaibetter/SaveContactAccountActivity.kt
@@ -1,5 +1,6 @@
 package com.example.finalprojectaibetter
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
@@ -9,6 +10,7 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.finalprojectaibetter.databinding.ActivitySaveContactAccountBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
@@ -19,6 +21,7 @@ class SaveContactAccountActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySaveContactAccountBinding
     private var db = Firebase.firestore
     private val userId by lazy { FirebaseAuth.getInstance().currentUser?.uid.orEmpty() }
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySaveContactAccountBinding.inflate(layoutInflater)
@@ -34,40 +37,65 @@ class SaveContactAccountActivity : AppCompatActivity() {
                     .whereEqualTo("userId", userId)
                     .get()
                     .addOnSuccessListener { documents ->
-                        for (document in documents) {
-                            messageOnSuccess.apply {
+                        if (documents.isEmpty) {
+                            messageOnSuccess.visibility = View.GONE
+                            messageOnFailure.apply {
                                 visibility = View.VISIBLE
+                                text = "User is already a contact."
                             }
-                            userFirstName.apply {
-                                text = document.data["userFirstName"].toString()
-                                visibility = View.VISIBLE
-                            }
-                        }
-                        addContactButton.setOnClickListener {
+                        } else {
+                            messageOnFailure.visibility = View.GONE
                             for (document in documents) {
-                                Log.d(TAG, "document data = ${document.data}")
-                                addContact(document.data, document.id)
-                                Handler(Looper.getMainLooper()).postDelayed({navigateTo(MainScreenActivity::class.java)} , 2000)
-                                return@setOnClickListener
+                                val userName = document.data["userFirstName"]
+                                val lastName = document.data["userLastName"]
+                                messageOnSuccess.apply {
+                                    visibility = View.VISIBLE
+                                    text = "Would you like to add $userName $lastName as a contact?"
+                                }
+                                addContactButton.setOnClickListener {
+                                    checkIfContactExists(document.id, document.data)
+                                    Log.d(TAG, "document data = ${document.data}")
+                                }
                             }
                         }
                     }
                     .addOnFailureListener {
+                        messageOnSuccess.visibility = View.GONE
                         Log.d(TAG, "Error getting documents: ", it)
                         messageOnFailure.apply {
                             visibility = View.VISIBLE
                             Handler(Looper.getMainLooper()).postDelayed({ visibility = View.GONE } , 4000)
                         }
                     }
-
-                addContactButton.setOnClickListener {
-                    navigateTo(MainScreenActivity::class.java)
-                }
             }
         }
     }
     private fun Context.navigateTo(destination: Class<*>) {
         startActivity(Intent(this, destination))
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun checkIfContactExists(documentId: String, user: MutableMap<String, Any>) {
+        db.collection("users")
+            .document(userId)
+            .collection("Contact")
+            .document(documentId)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    binding.messageOnFailure.apply {
+                        visibility = View.VISIBLE
+                        Handler(Looper.getMainLooper()).postDelayed({ visibility = View.GONE } , 4000)
+                    }
+                } else {
+                    addContact(user, documentId)
+                    Handler(Looper.getMainLooper()).postDelayed({navigateTo(MainScreenActivity::class.java)} , 2000)
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "User not Found", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "Error checking if contact exists: ", it)
+            }
     }
 
     private fun addContact(user: MutableMap<String, Any>, documentId: String) {
